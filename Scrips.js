57a022e140889449bc31c8e3f6a9b7385c5728bb
Ju4n000 <162451989+Ju4n000@users.noreply.github.com>
2024-03-20 11:48:53 -0300

    Add files via upload

---
+++ Scrips.js
@@ -0,0 +1,17 @@
+let Saludo = "Hola cabros del feisbuk"
+let Intro = "Era hace una vez......."
+
+let Nombre = "Juan Diego"
+let usuario = "PedritoAlcachofas"
+
+let Edad = 16
+let Estatura = 1.75
+
+let MayoríaEdad = false
+let Rockero = true
+
+let Numeritos = [23,50,1]
+let Datos = [a,b,c]
+ 
+let Usuario = {Nombre: "JD", Edad: "16", Estatura: 1.75}
+let Objeto = {Circulo: "Esfera", Triangulo: "pirámide", Cuadradaso: "Cubo"}
